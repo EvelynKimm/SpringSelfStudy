130ab5f4824de652295f3b098152bd6c8624ca41
EvelynKimm <evekimmy09@gmail.com>
2023-12-29 23:28:25 +0900

    initial commit

--- 23SpringMvc1/servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ 23SpringMvc1/servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -18,8 +18,7 @@ import java.io.IOException;
 @WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse
-            response) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("[전체 파라미터 조회] - start");
         request.getParameterNames().asIterator()
                 .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
