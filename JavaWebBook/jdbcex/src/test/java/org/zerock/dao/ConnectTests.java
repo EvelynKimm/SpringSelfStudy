f607178dd0fd6426473571f9da6d87512dde691b
Evelyn <108613992+EvelynKimm@users.noreply.github.com>
2024-01-21 18:15:23 +0900

    Upload JavaWeb chapter2 code
    자바 웹개발 워크북 Chap 2 코드 작성

---
+++ JavaWebBook/jdbcex/src/test/java/org/zerock/dao/ConnectTests.java
@@ -0,0 +1,44 @@
+package org.zerock.dao;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// 자바 코드를 이용해 설치된 MariaDB와 연결을 확인하는 용도의 코드
+public class ConnectTests {
+
+    @Test
+    public void testConnection() throws Exception {
+        Class.forName("org.mariadb.jdbc.Driver");
+
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mariadb://localhost:3306/webdb",
+                "webuser",
+                "webuser");
+
+        Assertions.assertNotNull(connection);
+        connection.close();
+    }
+
+    @Test
+    public void testHikariCP() throws Exception {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("org.mariadb.jdbc.Driver");
+        config.setJdbcUrl("jdbc:mariadb://localhost:3306/webdb");
+        config.setUsername("webuser");
+        config.setPassword("webuser");
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        HikariDataSource ds = new HikariDataSource(config);
+        Connection connection = ds.getConnection();
+
+        System.out.println(connection);
+
+        connection.close();
+    }
+}
